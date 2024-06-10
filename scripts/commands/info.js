b7d726a30d0d597ffa79e71ee869cf23148dba4c
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-06-10 12:46:04 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -1,1 +1,36 @@
-
+module.exports.config = {
+  name: "admin",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Rahad",
+  description: "",
+  prefix: true, 
+  category: "Admin information", 
+  usages: "/admin",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+var link =["https://i.postimg.cc/43mJvNkz/1698472327307.jpg",
+"https://i.postimg.cc/DwxJ3yCd/Screenshot-20231020-155148.jpg"];
+  
+var callback = () => api.sendMessage({body:`DO NOT TRUST THE BOT OPERATOR
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : Md fahim Islam 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : ☞♥♪✆😘😍🥵🤯 Fahim islam ☞♥♪✆😘😍🥵🤯
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Faridpur. Dhaka. Bangladesh 
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐤𝐨𝐦𝐮 𝐧𝐚
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𝐀𝐠𝐞           : 23+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+𝐆𝐦𝐚𝐢𝐥       : diya jabe na 🖕
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801892323664 
